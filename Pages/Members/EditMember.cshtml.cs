57ef8f4ac3779024937f0993d22508dcf017e356
juliefroeshoej <julf@itu.dk>
2022-12-19 10:29:38 +0100

    member pages

--- Pages/Members/EditMember.cshtml.cs
+++ Pages/Members/EditMember.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjektBoat.Interfaces;
+using ProjektBoat.Models;
 
 namespace ProjektBoat.Pages.Members
 {
     public class EditMemberModel : PageModel
     {
+        [BindProperty]
+        public Member Member { get; set; }
+
+        private IMemberRepository _repo;
+
+        public EditMemberModel(IMemberRepository repo)
+        {
+            _repo = repo;
+        }
         public void OnGet()
         {
+
+        }
+
+        public void OnPost()
+        {
+            _repo.AddMember(Member);
         }
     }
 }
